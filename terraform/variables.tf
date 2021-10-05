c9217736dc074a7b9bb4af3fd4c60782968023eb
Talha-Altair <cooltalha27@gmail.com>
2021-10-05 20:40:25 +0530

    tact modifications

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,7 +13,7 @@ variable "environment" {
 variable "region" {
   description = "(Optional) AWS Region."
   type = string
-  default = "us-east-1"
+  default = "ap-southeast-1"
 }
 
 variable "tags" {
